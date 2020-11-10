bb79b9997c3d24fa6cc43bd4d83118f75d2a663c
mr-antisecurity <73610922+mr-antisecurity@users.noreply.github.com>
2020-11-10 21:14:46 +0500

    update creds.py

--- creds.py
+++ creds.py
@@ -3,4 +3,4 @@ import os
 class Creds():
     TG_TOKEN = os.environ.get("BOT_TOKEN")
     TEAMDRIVE_FOLDER_ID = os.environ.get("TEAMDRIVE_FOLDER_ID")
-    TEAMDRIVE_ID = os.eviron.get("TEAMDRIVE_ID")
+    TEAMDRIVE_ID = os.environ.get("TEAMDRIVE_ID")
